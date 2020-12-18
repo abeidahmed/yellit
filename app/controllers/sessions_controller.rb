9e2be30c463aa48fdc44209396397f594585cba9
abeidahmed <abeidahmed92@gmail.com>
2020-12-18 19:45:40 +0530

    Make sign in api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    auth = Authentication.new(params)
+
+    if auth.authenticated?
+      login(auth.user)
+      redirect_to root_path
+    else
+      render json: { errors: { invalid: ["credentials"] } }, status: :bad_request
+    end
+  end
 end
