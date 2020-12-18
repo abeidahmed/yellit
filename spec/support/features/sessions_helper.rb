d17fa03ff5b69fd529290930477f9d217274dded
abeidahmed <abeidahmed92@gmail.com>
2020-12-18 19:20:56 +0530

    Make sign up form workable

--- spec/support/features/sessions_helper.rb
+++ spec/support/features/sessions_helper.rb
@@ -1,13 +1,5 @@
 module Features
   module SessionsHelper
-    def sign_up(full_name: "John Doe", email_address: "hello@exa.com", password: "secrettt")
-      visit new_user_path
-      fill_in "user[full_name]", with: "John Doe"
-      fill_in "user[email_address]", with: "hello@example.com"
-      fill_in "user[password]", with: "secrettt"
-      click_button "Sign up"
-    end
-
     def sign_in(user: nil)
       user = user || create(:user)
       visit new_session_path
