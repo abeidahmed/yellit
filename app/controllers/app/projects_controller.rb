13fb203c37663b0be9ee721b4b4dcd29e6f11964
abeidahmed <abeidahmed92@gmail.com>
2020-12-19 15:08:01 +0530

    Add project update action

--- app/controllers/app/projects_controller.rb
+++ app/controllers/app/projects_controller.rb
@@ -2,4 +2,20 @@ class App::ProjectsController < App::BaseController
   def edit
     skip_authorization
   end
+
+  def update
+    project = Project.find(params[:id])
+    authorize project
+
+    if project.update(project_params)
+      # do something
+    else
+      render json: { errors: project.errors }, status: :bad_request
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :website_url, :subdomain)
+  end
 end
--- spec/requests/app/projects_request_spec.rb
+++ spec/requests/app/projects_request_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "App::Projects", type: :request do
+  describe "#update" do
+    let(:membership) { create(:project_membership, :owner) }
+    let(:project) { membership.project }
+    before { login(membership.user) }
 
+    it "should update the project" do
+      patch app_project_path(project), params: { project: { name: "Hello", website_url: project.website_url, subdomain: project.subdomain } }
+
+      expect(project.reload.name).to eq("Hello")
+    end
+
+    it "should return error if project is invalid" do
+      patch app_project_path(project), params: { project: { name: "", website_url: project.website_url, subdomain: project.subdomain } }
+
+      expect(json.dig(:errors, :name)).to be_present
+    end
+  end
 end
