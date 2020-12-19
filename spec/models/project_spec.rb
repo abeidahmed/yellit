28ce11a394a091d143619125b1615f7b79728ef0
abeidahmed <abeidahmed92@gmail.com>
2020-12-19 14:41:22 +0530

    Add project model validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,42 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:project) }
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_most(255) }
+
+    it { should validate_length_of(:website_url).is_at_most(255) }
+
+    it { should validate_presence_of(:subdomain) }
+
+    it { should validate_length_of(:subdomain).is_at_most(63) }
+
+    it { should validate_uniqueness_of(:subdomain).case_insensitive }
+
+    it { should allow_value(
+                  "http://gmail.com/",
+                  "https://gmail.com/",
+                  "https://domain.gmail.com",
+                  "http://domain.gmail.com"
+                ).for(:website_url) }
+
+    it { should_not allow_value("http://gmail.com test", "https://gmail.com test").for(:website_url) }
+  end
+
+  describe "before save" do
+    it "should lowercase website_url" do
+      website_url = "https://google.com"
+      project     = create(:project, website_url: website_url.upcase)
+      expect(project.reload.website_url).to eq(website_url)
+    end
+
+    it "should lowercase subdomain" do
+      subdomain = "helloworld"
+      project   = create(:project, subdomain: subdomain.upcase)
+      expect(project.reload.subdomain).to eq(subdomain)
+    end
+  end
 end
