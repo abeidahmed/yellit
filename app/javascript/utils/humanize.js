2ad1cc91e55897f4fb25899c332a03f31457ca23
abeidahmed <abeidahmed92@gmail.com>
2020-12-19 09:51:25 +0530

    Add stimulus form controller

---
+++ app/javascript/utils/humanize.js
@@ -0,0 +1,10 @@
+import { capitalize } from './capitalize';
+
+export function humanize(string) {
+  if (typeof string !== 'string') {
+    throw new Error(`${string} should be of type string`);
+  }
+
+  const normalize = string.split('_').join(' ');
+  return capitalize(normalize);
+}
