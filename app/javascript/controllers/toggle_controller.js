3fc10eed71965b1ad8dbf3d955f552438f6a2ea9
abeidahmed <abeidahmed92@gmail.com>
2020-12-19 21:43:58 +0530

    Update toggle_controller to toggle html attributes

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,14 +3,47 @@ import ApplicationController from './application_controller';
 export default class extends ApplicationController {
   static targets = ['content'];
   static classes = ['toggle'];
+  static values = {
+    attribute: String,
+  };
 
   connect() {
     if (this.pageIsTurbolinksPreview) {
-      this.contentTarget.classList.add(this.toggleClass);
+      this.resetAttributes();
     }
   }
 
   toggle() {
-    this.contentTarget.classList.toggle(this.toggleClass);
+    this.toggleToggleClass();
+    this.toggleAttribute();
+  }
+
+  toggleToggleClass() {
+    if (this.hasToggleClass) {
+      this.contentTarget.classList.toggle(this.toggleClass);
+    }
+  }
+
+  toggleAttribute() {
+    if (this.hasAttributeValue) {
+      this.contentTarget.toggleAttribute(this.attributeValue);
+    }
+  }
+
+  resetAttributes() {
+    this.resetToggleClass();
+    this.resetHiddenAttribute();
+  }
+
+  resetToggleClass() {
+    if (this.hasToggleClass) {
+      this.contentTarget.classList.add(this.toggleClass);
+    }
+  }
+
+  resetHiddenAttribute() {
+    if (this.hasAttributeValue) {
+      this.contentTarget.setAttribute(this.attributeValue, '');
+    }
   }
 }
