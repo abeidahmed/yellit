5afefd78d6d8496265055e8f84b41a2dd87a1b14
abeidahmed <abeidahmed92@gmail.com>
2020-12-19 10:13:26 +0530

    Initialize projects edit action

---
+++ app/controllers/app/projects_controller.rb
@@ -0,0 +1,4 @@
+class App::ProjectsController < App::BaseController
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :users, only: %i(new create)
   resources :sessions, only: %i(new create)
   root "static_pages#home"
+
+  namespace :app do
+    scope "/settings/:id" do
+      root "projects#edit"
+    end
+  end
 end
