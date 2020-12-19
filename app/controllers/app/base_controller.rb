58897f36331fdb36afb25cb8a24368f32747ab6a
abeidahmed <abeidahmed92@gmail.com>
2020-12-19 14:58:39 +0530

    Initialize pundit

--- app/controllers/app/base_controller.rb
+++ app/controllers/app/base_controller.rb
@@ -1,3 +1,12 @@
 class App::BaseController < ApplicationController
   layout "app"
+
+  before_action :authenticate_user
+  after_action :verify_authorized, except: %i(index)
+  after_action :verify_policy_scoped, only: %i(index)
+
+  private
+  def authenticate_user
+    redirect_to new_session_path, alert: "Please signup or login to continue" unless user_signed_in?
+  end
 end
