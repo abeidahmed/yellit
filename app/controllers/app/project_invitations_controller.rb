4e570342307a55b54f064a1b872480bb42605c46
abeidahmed <abeidahmed92@gmail.com>
2020-12-20 17:29:38 +0530

    Make invitation update action

--- app/controllers/app/project_invitations_controller.rb
+++ app/controllers/app/project_invitations_controller.rb
@@ -6,6 +6,9 @@ class App::ProjectInvitationsController < App::BaseController
   end
 
   def update
+    membership = ProjectMembership.find(params[:id])
+    authorize membership, :roller?
 
+    membership.update(join_date: Time.zone.now)
   end
 end
--- spec/requests/app/project_invitations_request_spec.rb
+++ spec/requests/app/project_invitations_request_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "App::ProjectInvitations", type: :request do
+  describe "#update" do
+    it "should accept the invitation" do
+      membership = create(:project_membership, :pending)
+      login(membership.user)
+      patch app_project_invitation_path(membership), params: nil
 
+      expect(membership.reload.join_date).to be_present
+    end
+  end
 end
