dd14a7c8910aa4f16310a73b2391f2f0382cb822
abeidahmed <abeidahmed92@gmail.com>
2020-12-21 11:58:09 +0530

    Make project name dynamic

--- app/components/members/exit_modal_component.rb
+++ app/components/members/exit_modal_component.rb
@@ -1,3 +1,13 @@
 class Members::ExitModalComponent < ApplicationComponent
   with_content_areas :trigger
+
+  attr_reader :member
+
+  def initialize(member:)
+    @member = member
+  end
+
+  def project_name
+    member.project.name
+  end
 end
