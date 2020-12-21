a6af1e57211ac50b919c48214a1ee4769ec70846
abeidahmed <abeidahmed92@gmail.com>
2020-12-21 12:10:22 +0530

    Make member name dynamic

--- app/components/members/promote_modal_component.rb
+++ app/components/members/promote_modal_component.rb
@@ -1,3 +1,13 @@
 class Members::PromoteModalComponent < ApplicationComponent
   with_content_areas :trigger
+
+  attr_reader :member
+
+  def initialize(member:)
+    @member = member
+  end
+
+  def member_full_name
+    member.full_name
+  end
 end
