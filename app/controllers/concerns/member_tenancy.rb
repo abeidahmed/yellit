4083387d2a4b10037efa8b4f9011837ea6a5ddd0
abeidahmed <abeidahmed92@gmail.com>
2020-12-22 11:57:53 +0530

    Show banner_notification when user is removed from the team

--- app/controllers/concerns/member_tenancy.rb
+++ app/controllers/concerns/member_tenancy.rb
@@ -3,7 +3,7 @@ module MemberTenancy
 
   def check_member_tenancy_for(project)
     membership = current_user.find_project_membership(project)
-    return redirect_to app_projects_path, notice: "You have been removed from the team" if membership.nil?
+    return redirect_to app_projects_path, notice: { message: "You have been removed from the team", banner: true } if membership.nil?
     redirect_to app_project_invitation_path(membership.signed_id(purpose: :project_invitation)) if current_user.project_invite_pending?(project)
   end
 end
