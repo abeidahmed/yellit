7f9d1c306705b0e8408ef18625a61c7d87d08515
abeidahmed <abeidahmed92@gmail.com>
2020-12-22 11:44:37 +0530

    Refactor toast component

--- app/components/popovers/toast_component.rb
+++ app/components/popovers/toast_component.rb
@@ -1,6 +1,18 @@
 class Popovers::ToastComponent < ApplicationComponent
   attr_reader :type, :flash_hash
 
+  TOAST_PROPERTIES = {
+    success: { icon: "check", class: "Toast--success" },
+    notice: { icon: "info", class: "" },
+    alert: { icon: "stop", class: "Toast--error" }
+  }
+
+  BANNER_PROPERTIES = {
+    success: { class: "flash-success" },
+    notice: { class: "" },
+    alert: { class: "flash-error" }
+  }
+
   def initialize(type:, flash_hash:)
     @type        = type
     @flash_hash  = flash_hash
@@ -14,42 +26,15 @@ class Popovers::ToastComponent < ApplicationComponent
     flash_hash.fetch(:banner, false)
   end
 
-  def flash_class
-    case type
-    when "success"
-      "flash-success"
-    when "notice"
-      ""
-    when "alert"
-      "flash-error"
-    else
-      raise "Undefined flash type: #{type}"
-    end
+  def banner_class
+    BANNER_PROPERTIES.dig(type.to_sym, :class)
   end
 
   def toast_class
-    case type
-    when "success"
-      "Toast--success"
-    when "notice"
-      "Toast--info"
-    when "alert"
-      "Toast--error"
-    else
-      raise "Undefined toast type: #{type}"
-    end
+    TOAST_PROPERTIES.dig(type.to_sym, :class)
   end
 
   def toast_icon
-    case type
-    when "success"
-      "check"
-    when "notice"
-      "info"
-    when "alert"
-      "stop"
-    else
-      raise "Undefined toast type: #{type}"
-    end
+    TOAST_PROPERTIES.dig(type.to_sym, :icon)
   end
 end
