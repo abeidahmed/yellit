7050217c20f788b1ecc678d5f86f02c14628b6f4
abeidahmed <abeidahmed92@gmail.com>
2020-12-24 18:00:08 +0530

    Pass in hash key

--- app/javascript/controllers/active_param_controller.js
+++ app/javascript/controllers/active_param_controller.js
@@ -3,18 +3,22 @@ import ApplicationController from './application_controller';
 
 export default class extends ApplicationController {
   static targets = ['list'];
+  static values = {
+    key: String,
+  };
 
   connect() {
     this.setActive();
+    console.log(this.keyValue);
   }
 
   setActive() {
     const q = qs.parse(location.search);
 
     this.listTargets.forEach((t) => {
-      if (!t.value.length && (!Object.keys(q).length || !q.role)) {
+      if (!t.value.length && (!Object.keys(q).length || !q[this.keyValue])) {
         t.setAttribute('aria-checked', true);
-      } else if (t.value === q.role) {
+      } else if (t.value === q[this.keyValue]) {
         t.setAttribute('aria-checked', true);
       } else {
         t.setAttribute('aria-checked', false);
