e7220207278bc4d7f90a47e38f6c9614c78b9708
abeidahmed <abeidahmed92@gmail.com>
2020-12-24 18:20:25 +0530

    Fix toast flicker on page visit

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -4,13 +4,15 @@ export default class extends ApplicationController {
   static targets = ['content'];
 
   connect() {
-    if (!this.pageIsTurboPreview) {
-      this.contentTarget.classList.add('Toast--animateIn');
-
-      setTimeout(() => {
-        this.hide();
-      }, 5000);
+    if (this.pageIsTurboPreview) {
+      return this.element.remove();
     }
+
+    this.contentTarget.classList.add('Toast--animateIn');
+
+    setTimeout(() => {
+      this.hide();
+    }, 5000);
   }
 
   hide() {
