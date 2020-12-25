25681c0d43d011a90b4707909a55e558b2c7ab55
abeidahmed <abeidahmed92@gmail.com>
2020-12-25 09:42:36 +0530

    Add label model validation

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,3 +1,22 @@
 class Label < ApplicationRecord
   belongs_to :project
+
+  before_validation :normalize_hex_code
+
+  VALID_HEX_REGEX = /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/i
+
+  validates_presence_of :name
+  validates_length_of :name, maximum: 255
+  validates_uniqueness_of :name, case_sensitive: false, scope: :project_id
+  validates_format_of :text_color, :bg_color, with: VALID_HEX_REGEX, message: "is not a valid hex color code"
+
+  private
+  def normalize_hex_code
+    self.text_color = prefix_hash(self.text_color)
+    self.bg_color = prefix_hash(self.bg_color)
+  end
+
+  def prefix_hash(hex_code)
+    hex_code.chr == "#" ? hex_code : "##{hex_code}"
+  end
 end
--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:label) }
+
+  describe "associations" do
+    it { should belong_to(:project) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+
+    it { should validate_uniqueness_of(:name).scoped_to(:project_id).case_insensitive }
+
+    it { should validate_length_of(:name).is_at_most(255) }
+
+    it { should allow_value("#000", "#1f1f1F", "1f1f1F").for(:text_color) }
+
+    it { should allow_value("#000", "#1f1f1F", "1f1f1F").for(:bg_color) }
+
+    it { should_not allow_value("#F0h", "#afaf", "1f1#1F", "0000").for(:text_color) }
+
+    it { should_not allow_value("#F0h", "#afaf", "1f1#1F", "0000").for(:bg_color) }
+  end
 end
