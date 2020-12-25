5a9654aaecdade9df6de21e801a014d8623dd436
abeidahmed <abeidahmed92@gmail.com>
2020-12-25 22:27:52 +0530

    Update default tag color

--- db/migrate/20201225040018_create_labels.rb
+++ db/migrate/20201225040018_create_labels.rb
@@ -2,8 +2,8 @@ class CreateLabels < ActiveRecord::Migration[6.1]
   def change
     create_table :labels, id: :uuid do |t|
       t.string     :name,       null: false
-      t.string     :bg_color,   null: false, default: "#f6f8fa"
-      t.string     :text_color, null: false, default: "#24292e"
+      t.string     :bg_color,   null: false, default: "#0e8a16"
+      t.string     :text_color, null: false, default: "#ffffff"
       t.references :project,    null: false, foreign_key: true, type: :uuid
 
       t.timestamps
