d339dfdf91f1bb37dc1ca43af815ab4b4b97a676
abeidahmed <abeidahmed92@gmail.com>
2020-12-26 11:53:16 +0530

    Update form error handling to use turbo errors

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,9 +4,10 @@ import { formErrorHandler } from '../utils';
 export default class extends Controller {
   static targets = ['showError'];
 
-  onError(e) {
-    const [, , xhr] = e.detail;
-    const { errors } = JSON.parse(xhr.response);
+  async onError(e) {
+    const formData = await e.detail.formSubmission;
+    const res = await formData.result.fetchResponse.responseText;
+    const { errors } = JSON.parse(res);
 
     this.showErrorTargets.forEach((errorTarget) => {
       const errorType = errorTarget.getAttribute('data-form-error');
