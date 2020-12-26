82734865a6a616b223b2163e7dee1efe3b5ad4e3
abeidahmed <abeidahmed92@gmail.com>
2020-12-26 17:56:11 +0530

    Hide label create form on initial page load

--- spec/features/label_form_spec.rb
+++ spec/features/label_form_spec.rb
@@ -6,10 +6,10 @@ RSpec.feature "LabelForms", type: :feature do
     project    = membership.project
     sign_in(user: membership.user)
     visit app_labels_path(project)
-    within "#label-form" do
-      fill_in "Label name", with: "Just created"
-      fill_in "Color", with: "#000000"
-      click_button "Create"
+    within "#label-form", visible: false do
+      fill_in "Label name", visible: false, with: "Just created"
+      fill_in "Color", visible: false, with: "#000000"
+      click_button "Create", visible: false
     end
 
     expect(current_path).to eq(app_labels_path(project))
