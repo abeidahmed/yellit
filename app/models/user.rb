8d807203739c1032844ba93edcd24b4b329a8685
abeidahmed <abeidahmed92@gmail.com>
2020-12-27 11:39:03 +0530

    Forbid accepting full_name as anonymous

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,18 @@ class User < ApplicationRecord
 
   before_create :generate_auth_token
   before_save :normalize_email_address
-  before_validation :trim_white_space
+  before_validation :trim_white_spaces
 
   VALID_EMAIL_REGEX   = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   MIN_PASSWORD_LENGTH = 6
+  FULL_NAME_EXCLUSION = %w(anonymous)
 
   validates_presence_of :email_address, :full_name
   validates_uniqueness_of :email_address, case_sensitive: false
   validates_length_of :email_address, :full_name, maximum: 255
   validates_format_of :email_address, with: VALID_EMAIL_REGEX
   validates_length_of :password, minimum: MIN_PASSWORD_LENGTH, on: :create
+  validate :full_name_is_not_reserved
 
   private
   def generate_auth_token
@@ -28,9 +30,13 @@ class User < ApplicationRecord
     self.email_address = email_address.downcase
   end
 
-  def trim_white_space
+  def trim_white_spaces
     self.email_address = email_address.strip if email_address
     self.full_name     = full_name.strip if full_name
+  end
+
+  def full_name_is_not_reserved
+    errors.add(:full_name, "can't be #{full_name}") if FULL_NAME_EXCLUSION.include? full_name.to_s.strip.downcase
   end
 
   def generate_token(column)
