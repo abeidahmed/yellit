428793432574a560e9e97e6a91dd8ad35924157d
abeidahmed <abeidahmed92@gmail.com>
2020-12-28 17:38:08 +0530

    Add section model validations

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,5 @@
 class Section < ApplicationRecord
   belongs_to :post
+
+  validates_length_of :body, maximum: 5000
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Section, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:section) }
+
+  describe "associations" do
+    it { should belong_to(:post) }
+  end
+
+  describe "validations" do
+    it { should validate_length_of(:body).is_at_most(5000) }
+  end
 end
