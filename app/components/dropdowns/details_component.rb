9d8a8cac473ebc91a3916cf5c31b302ea28ea133
abeidahmed <abeidahmed92@gmail.com>
2020-12-28 13:51:24 +0530

    Refactor dropdown details component

--- app/components/dropdowns/details_component.rb
+++ app/components/dropdowns/details_component.rb
@@ -1,12 +1,11 @@
 class Dropdowns::DetailsComponent < ApplicationComponent
-  with_content_areas :body
+  with_content_areas :body, :trigger
 
   attr_reader :button_title, :title, :position
 
-  def initialize(button_title:, title: nil, position: "right")
-    @button_title = button_title
-    @title       = title
-    @position    = position
+  def initialize(title: nil, position: "right")
+    @title    = title
+    @position = position
   end
 
   def dropdown_position
